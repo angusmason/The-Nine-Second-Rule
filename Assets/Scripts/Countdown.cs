d95bd728dcda5226739a19fa0e09a7d35425452e
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-02-22 13:37:55 +1100

    Flash the countdown

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -7,11 +7,11 @@ namespace TNSR
     public class Countdown : MonoBehaviour
     {
         const int TimeAvailable = 9;
+        const float flashRate = 0.2f;
         DateTime startTime;
         [SerializeField] TextMeshProUGUI countUpText;
         [SerializeField] TextMeshProUGUI countDownText;
         public event EventHandler TimeUp;
-
         bool counting;
         public TimeSpan Time { get; set; }
 
@@ -35,15 +35,14 @@ namespace TNSR
 
         void UpdateText()
         {
+            var timeLeft = (float)(TimeSpan.FromSeconds(TimeAvailable) - Time)
+                .TotalSeconds;
             countUpText.text = Time.ToString(@"s\.ff");
             countDownText.text = TimeSpan
-                .FromSeconds(
-                    Mathf.Round(
-                        (float)(TimeSpan.FromSeconds(TimeAvailable) - Time)
-                            .TotalSeconds
-                    * 100) / 100
-                )
+                .FromSeconds(Mathf.Round(timeLeft * 100) / 100)
                 .ToString(@"s\.ff");
+
+            countDownText.color = timeLeft < 3 && Mathf.Round(timeLeft / flashRate) % 2 == 0  ? Color.red : Color.white;
         }
 
         public void ResetTime()
