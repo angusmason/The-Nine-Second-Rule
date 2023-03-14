29b8ae1c568772d8b4913af2294faabcfd8b569a
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-03-15 08:54:02 +1100

    animate finish (#36)

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    Transform sprite;
+    float rotation;
+    Vector2 randomOffset;
+
+    void Start()
+    {
+        sprite = transform.Find("Sprite");
+        rotation = Random.Range(-100, 100);
+        randomOffset = new(
+            Random.Range(-2 * Mathf.PI, 2 * Mathf.PI),
+            Random.Range(-2 * Mathf.PI, 2 * Mathf.PI)
+        );
+    }
+
+    void Update()
+    {
+        sprite.Rotate(0, 0, rotation * Time.deltaTime);
+        sprite.transform.localPosition = new Vector3(
+            Mathf.Sin(Time.time * 2 + randomOffset.x) * 0.5f,
+            Mathf.Sin(Time.time * 2 + randomOffset.y) * 0.5f,
+            0
+        );
+    }
+}
